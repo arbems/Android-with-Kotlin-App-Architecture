21f56fe585bdcacac218a7de2b0b108929155ecd
Alberto Moreno Simón <>
2020-08-17 19:17:17 +0200

    no message

--- Guía de arquitectura de apps/Arquitectura Android MVP/Readme.md
+++
@@ -1,8 +0,0 @@
-# Android con Kotlin - Arquitectura Android MVP
-
-
-
-
-## Attribution
-
-This code was created by [arbems](https://github.com/arbems) in 2020.
--- Guía de arquitectura de apps/Arquitectura Android MVVM/app/src/main/java/com/arbems/mvvmbasic/data/Quote.kt
+++
@@ -1,9 +0,0 @@
-package com.arbems.mvvmbasic.data
-
-
-data class Quote(val quoteText: String,
-                 val author: String) {
-    override fun toString(): String {
-        return "$quoteText - $author"
-    }
-}
